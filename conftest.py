8fd3a3c07cd9f649131720802516aca0ab10ff74
Yass <yassineboulakhrif75@gmail.com>
2026-06-03 17:18:04 +0200

    Driver fixture done

--- conftest.py
+++ conftest.py
@@ -1,24 +1,52 @@
+#All imports needed
 import pytest
-from selenium import webdriver
-# Global fixtures for Pytest
 
+from selenium import webdriver
+from selenium .webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
+
+import os
+from dotenv import load_dotenv
+
+import csv
+
+
+
+# ---------- Calling load_dotenv() function to load environment variables (not native with python) ---------- #
+
+load_dotenv()
+
+
+
+# ---------- Global fixtures for Pytest ---------- #
 
 #Driver - fixture for Selenium tests
-@pytest.fixture
+@pytest.fixture(scope="function")
 def driver():
-    driver = webdriver.Chrome()
+    
+    options = Options()
+    
+    #if CI environment variable is set to "true", run in headless mode
+    if os.getenv("CI"):
+        options.add_argument("--headless")               #To make it run in headless mode for CI environment
+        options.add_argument("--no-sandbox")             #To Bypass OS security model
+        options.add_argument("--disable-dev-shm-usage")  #
+        
+    driver = webdriver.Chrome(options=options)
     yield driver
     driver.quit()
 
 
 #Base_url - fixture for test URLs
-@pytest.fixture
+@pytest.fixture(scope="session")
 def base_url():
     return "https://demowebshop.tricentis.com/"
 
 
 #Logged-in user fixture for authentication tests (required for cart and checkout tests)
-@pytest.fixture
+@pytest.fixture(scope="function")
 def logged_in_user(driver, base_url):
     driver.get(base_url + "login")
     driver.find_element("id", "Email").send_keys("testuser@example.com")
