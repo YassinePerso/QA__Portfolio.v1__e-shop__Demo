e28ad87a2b15195cac4799548f0bb3065171a67d
Yass <yassineboulakhrif75@gmail.com>
2026-06-04 20:43:52 +0200

    FEAT: login_page.py file DONE

--- pages/login_page.py
+++ pages/login_page.py
@@ -32,12 +32,12 @@ class LoginPage (BasePage):
         
     #If test case is negative, get the error message text and return it
     def get_error_message(self):
-        error_message_locator = (By.XPATH, "div[class='validation-summary-errors'] span")      #Locator for error message element
+        error_message_locator = (By.CSS_SELECTOR, "div[class='validation-summary-errors'] span")      #Locator for error message element
         return self.get_element_text(error_message_locator)      #Using .get_element_text() function of BasePage class to find error message element and return its text
     
     
     #Checking if login is succesful by checking if the logout link is visible since the URL does not change upon successful login
     def is_login_successful(self):
         #Locator for logout link, which is only visible when login is successful
-        logout_link_locator = (By.XPATH, "//a[@class='ico-logout']")
+        logout_link_locator = (By.CSS_SELECTOR, "a[class='ico-logout']")
         return self.is_element_visible(logout_link_locator)     #Using .is_element_visible() function of BasePage class to check if logout link is visible, which indicates that login was successful
